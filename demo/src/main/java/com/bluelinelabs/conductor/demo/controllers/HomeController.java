1110a5503131e6c332bde77dfde29522a93868f1
Christian H.C <chris6647@gmail.com>
2017-06-19 10:50:21 +0200

    Adds BottomNavigationController to the demo app
    Showcasing a BottomNavigationView implementation with separate,
    retained backstacks for each MenuItem.

--- demo/src/main/java/com/bluelinelabs/conductor/demo/controllers/HomeController.java
+++ demo/src/main/java/com/bluelinelabs/conductor/demo/controllers/HomeController.java
@@ -52,6 +52,7 @@ public class HomeController extends BaseController {
         MULTIPLE_CHILD_ROUTERS("Multiple Child Routers", R.color.deep_orange_300),
         MASTER_DETAIL("Master Detail", R.color.grey_300),
         DRAG_DISMISS("Drag Dismiss", R.color.lime_300),
+        BOTTOM_NAVIGATION("Bottom Navigation", R.color.purple_600),
         EXTERNAL_MODULES("Bonus Modules", R.color.teal_300);
 
         String title;
@@ -200,6 +201,12 @@ public class HomeController extends BaseController {
                 getRouter().pushController(RouterTransaction.with(new DragDismissController())
                         .pushChangeHandler(new FadeChangeHandler(false))
                         .popChangeHandler(new FadeChangeHandler()));
+                break;
+            case BOTTOM_NAVIGATION:
+                getRouter().pushController(RouterTransaction.with(new BottomNavigationController
+                                                                          (R.menu.navigation))
+                                                            .pushChangeHandler(new FadeChangeHandler())
+                                                            .popChangeHandler(new FadeChangeHandler()));
                 break;
             case EXTERNAL_MODULES:
                 getRouter().pushController(RouterTransaction.with(new ExternalModulesController())
